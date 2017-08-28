18744abea5649d0d3300e3e09393fed125d8ef58
Ongchangco Princess Diana (ERNI) <onp@erni.ph>
2017-08-28 21:09:49 +0800

    bootstrap test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Demo1Component } from './demo/bootstrap/demo1/demo1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Demo1Component
   ],
   imports: [
     BrowserModule
