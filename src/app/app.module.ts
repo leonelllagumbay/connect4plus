ff5beb6bd74bc840095dbe9cb644acf32d521bb4
Ongchangco Princess Diana (ERNI) <onp@erni.ph>
2017-08-30 15:05:43 +0800

    complete bootstrap prep 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { Demo1Component } from './demo/bootstrap/demo1/demo1.component';
@@ -10,7 +12,9 @@ import { Demo1Component } from './demo/bootstrap/demo1/demo1.component';
     Demo1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/demo/bootstrap/demo1/demo1.component.ts
+++ src/app/demo/bootstrap/demo1/demo1.component.ts
@@ -1,5 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import * as jQuery from 'jquery';
+import { Component, OnInit, Input } from '@angular/core';
+//import * as $ from 'jquery';
+import {NgbModule, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+const now = new Date();
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
 @Component({
   selector: 'app-demo1',
@@ -8,10 +23,92 @@ import * as jQuery from 'jquery';
 })
 export class Demo1Component implements OnInit {
 
-  constructor() { }
+  // Typeahead
+  public model2: any;
 
+  search = (text$: Observable<string>) =>
+    text$
+      .debounceTime(200)
+      .distinctUntilChanged()
+      .map(term => term.length < 2 ? []
+        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10));
+        
   ngOnInit() {
     
   }
+  
+  ngAfterViewInit() {
+    
+  }
 
+  @Input()
+  public alerts: Array<IAlert> = [];
+
+  private backup: Array<IAlert>;
+
+  constructor() {
+    this.alerts.push({
+      id: 1,
+      type: 'success',
+      message: 'This is an success alert',
+    }, {
+      id: 2,
+      type: 'info',
+      message: 'This is an info alert',
+    }, {
+      id: 3,
+      type: 'warning',
+      message: 'This is a warning alert',
+    }, {
+      id: 4,
+      type: 'danger',
+      message: 'This is a danger alert',
+    }, {
+      id: 5,
+      type: 'primary',
+      message: 'This is a primary alert',
+    }, {
+      id: 6,
+      type: 'secondary',
+      message: 'This is a secondary alert',
+    }, {
+      id: 7,
+      type: 'light',
+      message: 'This is a light alert',
+    }, {
+      id: 8,
+      type: 'dark',
+      message: 'This is a dark alert',
+    });
+    this.backup = this.alerts.map((alert: IAlert) => Object.assign({}, alert));
+  }
+
+  public closeAlert(alert: IAlert) {
+    const index: number = this.alerts.indexOf(alert);
+    this.alerts.splice(index, 1);
+  }
+
+  public reset() {
+    this.alerts = this.backup.map((alert: IAlert) => Object.assign({}, alert));
+  }
+  
+  // Datepicker
+  model: NgbDateStruct;
+  date: {year: number, month: number};
+
+  selectToday() {
+    this.model = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
+  }
+  
+  // Rating
+  currentRate = 3.14;
+  
+  // Timepicker
+  time = {hour: 13, minute: 30};
+}
+
+export interface IAlert {
+  id: number;
+  type: string;
+  message: string;
 }
